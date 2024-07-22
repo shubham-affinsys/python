1f5793ca7f554b983938adbc12d4c46a5158d833
shubham-affinsys <shubham.chuahan@affinssys.com>
2024-07-22 10:09:10 +0530

    time scheduling

---
+++ time_schedulting/ts.py
@@ -0,0 +1,42 @@
+import time
+import sys
+import cProfile
+# sys.set_int_max_str_digits(456570)
+# def calProd():
+#     prod=1
+#     for i in range(1,100000):
+#         prod=prod*i
+#     # return prod
+
+# startTime = time.time()
+# prod = calProd()
+# endTime = time.time()
+
+# # print(len(str(prod)))
+# print(endTime-startTime)
+
+# cProfile.run(prod())
+
+
+# def greet():
+#     print("hello")
+# def bye():
+#     print("bye")
+
+# greet()
+# time.sleep(1)
+
+# bye()
+# time.sleep(2)
+
+# greet()
+# time.sleep(1)
+# bye()
+
+
+#round time
+# now = time.time()
+# print(now)
+# print(round(now,2))
+
+
